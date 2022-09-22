d754ed853eaaa8dd7913a99d40a462022ed9d864
DmityFilchev <filchev_dmitry@mail.ru>
2022-09-22 13:46:09 +0300

    Добавьте файлы проекта.

---
+++ Task23.2/Task23.2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+///#define SPRING 1
+//#define SUMMER 1
+//#define AUTUMN 1
+#define WINTER 1
+
+#if SPRING
+int main()
+{
+    std::cout << "Spring"<<std::endl;
+}
+#endif
+#if SUMMER
+int main()
+{
+    std::cout << "Summer" << std::endl;
+}
+#endif
+#if AUTUMN
+int main()
+{
+    std::cout << "Autumn" << std::endl;
+}
+#endif
+#if WINTER
+int main()
+{
+    std::cout << "Winter" << std::endl;
+}
+#endif
